c22478c415c67b54595aa82c6f10a21293b3d46e
gautier.rols <gautier.rols@gmail.com>
2014-01-09 10:21:04 +0000

    TriatementFichier Clean

--- pfe-eclipse/src/TraitementFichier.java
+++ pfe-eclipse/src/TraitementFichier.java
@@ -7,27 +7,26 @@ import java.util.ArrayList;
 
 public class TraitementFichier {
 
+	
+	
 	// Ne pas mettre les extensions de fichiers
+
 	//private static String fileAdress = "C:\\Users\\Gaï¿½tch\\Desktop\\PFE\\Workspace\\pfe-eclipse\\HistoriqueZeroCoupons2";
 	private static String fileAdress = "/Users/david/Desktop/Polytech/MAM5/PFE/ProjetsEclypse/pfe-eclipse/HistoriqueZeroCoupons2";
 
 
 
+	
+	
 	public static void main (String[] args){
-
-		//	String aa = "12M";
-		//System.out.println(ConvertDate(aa) + ";");
-		//		
-		//		String aa = "0,11224";
-		//	aa.replace('0', '2');
-
-
-		
-
 		//	traitementCsv();
 	}
 
 
+	/*
+	 * Permet de convertir un fichier CSV en fichier txt 
+	 * afin de pouvoir manipuler les données
+	 */
 	public static void traitementCsv(){ 
 
 		try {
@@ -47,31 +46,22 @@ public class TraitementFichier {
 				System.out.println(line);
 
 				if(line.startsWith("Level")){ // Date avec 1M 2M etc...
-					//					System.out.println("Cette ligne commence par un Level ");
 					lArray = line.split(";");
 					for(int i=2; i< lArray.length; i++ ){ // On ne copie pas les 2 premiers ï¿½lements
 						lArray[i]= ConvertDate(lArray[i]);
 						bw.write((String) lArray[i] +";");
-
-
 					}		
-
 				}
 
 				else if(line.startsWith("0")){
-					//					System.out.println("Cette ligne commence par un 0 ");
 					lArray = line.split(";");
 					for(int i=1; i< lArray.length; i++ ){ // On ne copie pas le premier ï¿½lï¿½ment
 						bw.write((String) lArray[i] +";");
-
-
 					}		
-
 				}
 
 				// Premiere ligne
 				else if(line.startsWith("")){
-					//					System.out.println("Cette ligne commence par un vide ");
 					lArray = line.split(";");
 					for(int i=2; i< lArray.length; i++ ){ // On ne copie pas les 2 premiers ï¿½lements
 						bw.write((String) lArray[i] +";");
@@ -80,7 +70,6 @@ public class TraitementFichier {
 				}
 
 			}
-
 
 			bw.close(); 
 
@@ -91,16 +80,16 @@ public class TraitementFichier {
 
 
 	}
-	
+	/*
+	 * Permet de convertir une date de la forme 1M en 1/12
+	 * 	
+	 */
 	public static String ConvertDate(String dateM){
 		String date = null;
 		int length = dateM.length();
 		date = dateM.substring(0, length-1);
 		int dateInt = Integer.parseInt(date); 
 		date += "/12";
-		//		int dateInt = Integer.parseInt(date); 
-		//		dateInt *= 12;
-		//		date = Integer.toString(dateInt);
 		return date;
 	}
 
